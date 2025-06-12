4fc569befb3b1aa00505bd0446093b55ab2c69f9
aaa <aaa@aaa.com>
2025-06-12 17:45:41 +0900

    画面遷移仕掛中

--- src/main/java/com/example/demo/authentication/AuthController.java
+++ src/main/java/com/example/demo/authentication/AuthController.java
@@ -1,13 +1,26 @@
 package com.example.demo.authentication;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class AuthController {
+	
+	@Autowired
+	HttpSession session;	
+	
+	@GetMapping("/auth/signup")
+	public String entry() {
+		return "user/signup";
+	}
+	
 	@GetMapping("/")
 	public String index() {
+		session.invalidate();
 		return "auth/signin";
 	}
 	
